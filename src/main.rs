0068751a2ab02d461e30c04938f7c4a597663887
Tom <tomowl@pm.me>
2024-07-29 20:57:34 -0300

    made it more modular remove old assets
    Signed-off-by: Tom <tomowl@pm.me>

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::process::Output;
 use image::{open, GenericImageView, ImageFormat, DynamicImage, imageops::FilterType, imageops::crop_imm};
+use std::{env, path::Path};
 
-fn resize_image(path: &str, width: u32, height: u32) -> DynamicImage{
-    let image = open(path).expect("Failed to open image");
+fn resize_image(image: &DynamicImage, width: u32, height: u32) -> DynamicImage{
     image.resize(width, height, FilterType::Lanczos3)
 }
 
@@ -10,22 +10,20 @@ fn save_image(image: &DynamicImage, output_path: &str) {
     image.save_with_format(output_path, ImageFormat::Png).expect("Failed to save image");
 }
 
-fn rotate_image(path: &str, degrees: u32) -> DynamicImage{
-    let img = open(path).expect("Failed to load image");
+fn rotate_image(img: &DynamicImage, degrees: u32) -> DynamicImage{
     match degrees {
         90 => img.rotate90(),
         180 => img.rotate180(),
         270 => img.rotate270(),
         _ => {
             eprintln!("Unsupported rotation angle!");
-            img
+            img.clone()
         }
     }
 }
 
 
-fn resize_image_maintain_ratio(path: &str, new_width: Option<u32>, new_height: Option<u32>) -> DynamicImage {
-    let img = open(path).expect("Failed to open image");
+fn resize_image_maintain_ratio(img: &DynamicImage, new_width: Option<u32>, new_height: Option<u32>) -> DynamicImage {
     let (width, height) = img.dimensions();
     // calculate new dimensions while maintaining aspect ratio
     let ratio = width as f32 / height as f32;
@@ -47,27 +45,36 @@ fn crop_image(img: &DynamicImage, x: u32, y: u32, width: u32, height: u32) -> Dy
 
 
 fn main() {
-    let path = "assets/sample_img.jpg";
-    // let outpath = "assets/resized.png";
-    // let outrotatepath = "assets/rotated.png";
-    let outpath = "assets/resized_ratio.png";
-    let croppedpath = "assets/cropped.png";
-
-    // let resized_ratio = resize_image_maintain_ratio(path, Some(512), Some(246));
-    // save_image(&resized_ratio, outpath);
-
-    let img = open(path).expect("Oops!");
-    let crop_im = crop_image(&img, 50, 500, 500, 500);
-    save_image(&crop_im, croppedpath);
-    // let resized_image = resize_image(path, 512, 512);
-    // let rotated_image = rotate_image(path, 270);
-    // save_image(&rotated_image, outrotatepath);
-
-    // let img = open("assets/sample_img.jpg").expect("Failure opening image");
-    //     let (width, height) = img.dimensions();
-    // img.save_with_format(
-    //     "assets/sample_img.png", ImageFormat::Png).expect("Save to save as PNG");
-    // img.save_with_format(
-        // "assets/sample_img.webp", ImageFormat::WebP).expect("Save to save as WebP");
-
+    let args: Vec<String> = env::args().collect();
+    let operation = &args[1];
+    let path = Path::new(&args[2]);
+    let route = path.parent().unwrap().to_str().unwrap();
+    let extension = "png";
+    let outpath = format!("{}/{}.{}", route, operation, extension);
+    let img= open(path).expect("oops couldn't open the file!");
+    match operation.as_str() {
+        "crop" => {
+            let cropped = crop_image(&img, 50, 500, 500, 500);
+            save_image(&cropped, &outpath);
+        },
+        "rotate" => {
+            let rotated_image = rotate_image(&img, 270);
+            save_image(&rotated_image, &outpath);
+        },
+        "resize" => {
+            let resized_image = resize_image(&img, 512, 512);
+            save_image(&resized_image, &outpath);
+        },
+        "resize-ratio" => {
+            let resized_ratio = resize_image_maintain_ratio(&img, Some(512), Some(246));
+            save_image(&resized_ratio, &outpath);
+        },
+        "to-png" => {
+            img.save_with_format(
+                outpath, ImageFormat::Png).expect("Save to save as PNG");
+        },
+        _ => {
+            eprintln!("We only support: [crop, rotate, resize, resize-ratio, formats]");
+        }
+    }
 }
